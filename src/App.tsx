32ee845bbfa0f1d7238fb7f20186947aa9c9684e
Javad Farokhi <javad.me1376@gmail.com>
2024-03-03 20:24:35 +0100

    Create responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import "./App.css";
 
 function App() {
-  return <>
-    <div>Hello World!</div>
-  </>;
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={12} bgcolor={"gold"}>Header</Grid>
+        <Grid item sx={{display: {xs: "none" , md: "block"}}} md={3} bgcolor={"blueviolet"}>SideBar</Grid>
+        <Grid item  xs={12} md={9} bgcolor={"royalblue"}>Main</Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
