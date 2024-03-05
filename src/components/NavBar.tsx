de3ac5b84224b7696184edc20bc515a3fc973711
Javad Farokhi <javad.me1376@gmail.com>
2024-03-05 13:06:29 +0100

    Add login route

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,15 @@
 import EventIcon from "@mui/icons-material/Event";
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
-import { useState } from "react";
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function NavBar() {
+  const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("access_token") !== null) {
+      setIsAuth(true);
+    }
+  }, [isAuth]);
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -22,10 +29,18 @@ function NavBar() {
               letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
+              flexGrow: 1
             }}
           >
             Evently
           </Typography>
+          <Box>
+            {isAuth ? (
+              <Button variant="outlined" href="/logout">Logout</Button>
+            ) : (
+              <Button variant="contained" href="/login">Login</Button>
+            )}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
