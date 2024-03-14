a03c83ddaa15563871a1c0e1c0a7aa67c4ce29d7
Javad Farokhi <javad.me1376@gmail.com>
2024-03-14 15:35:27 +0100

    Add header to Events request

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -1,20 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import authHeader from "../services/auth-header";
 
 interface Event {
   id: number;
   title: string;
 }
 
-interface FetchResponse {
+export interface FetchResponse {
   count: number;
   results: Event[];
 }
 
 function EventsList() {
   const fetchEvents = () =>
-    axios.get<FetchResponse>("http://127.0.0.1:8000/events/").then((res) => res.data.results);
+    axios.get<FetchResponse>("http://127.0.0.1:8000/events/", { headers: authHeader() }).then((res) => res.data.results);
 
   const {data: events} = useQuery<Event[], Error>({
     queryKey: ['events'],
