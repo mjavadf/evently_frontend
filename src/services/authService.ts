22470776dd76ffc0037085365674f6cdf6e05f47
Javad Farokhi <javad.me1376@gmail.com>
2024-03-27 21:51:20 +0100

    decrypt access token

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+interface JWTPayload {
+  user_id: number;
+  exp: number;
+  username: string;
+}
 
 const API_URL = "http://127.0.0.1:8000/auth/";
 
 export const login = (username: string, password: string) => {
-
   return axios
     .post(API_URL + "jwt/create/", {
       username,
@@ -12,17 +18,16 @@ export const login = (username: string, password: string) => {
     .then((response) => {
       if (response.data.access) {
         localStorage.setItem("user", JSON.stringify(response.data));
-        return response.data
+        return response.data;
       }
 
-      return response.data
+      return response.data;
     });
 };
 
 export const logout = () => {
   localStorage.removeItem("user");
 };
-
 
 export const register = (
   username: string,
@@ -32,19 +37,31 @@ export const register = (
   lastName: string,
   phoneNumber: string
 ) => {
-  return axios.post(API_URL + "users/", {
-    username,
-    email,
-    password,
-    firstName,
-    lastName,
-    phoneNumber,
-  }).then(response => {return response.data});
+  return axios
+    .post(API_URL + "users/", {
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+      phoneNumber,
+    })
+    .then((response) => {
+      return response.data;
+    });
 };
 
-export const getCurrentUser = () => {
+export const getCurrentUser: () => JWTPayload | null = () => {
   const userStr = localStorage.getItem("user");
-  if (userStr) return JSON.parse(userStr);
+
+  if (userStr) {
+    let payload: JWTPayload = jwtDecode(JSON.parse(userStr).access);
+    return {
+      user_id: payload.user_id,
+      exp: payload.exp,
+      username: payload.username,
+    }
+  };
 
   return null;
 };
